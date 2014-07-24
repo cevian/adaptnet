29bdf81cc00b6b5de99773cf90527ca1bc33d5e8
Matvey Arye <arye@cs.princeton.edu>
2014-07-24 12:24:12 -0400

    put in sanity check

--- server/shaper.sh
+++ server/shaper.sh
@@ -44,15 +44,24 @@
 # 2) Turning on shaping on eth0 in general on sns is dangerous since nfs also uses it. This can cause unexplained slowdown.
 #    Thus this script only shapes traffic for one port (PORT)
 
+OFFLOAD=`ethtool -k eth0| grep tcp-segmentation-offload |awk '{print $2}'` 
+if [ $OFFLOAD != "off" ]
+then
+  echo "Offloading is on!. Can't run shaper."
+  echo "Please run ethtool -K eth0 tso off; ethtool -K eth0 gro off; ethtool -K eth0 gso off."
+  exit
+fi
+
+
 
 # Name of the traffic control command.
 TC=/sbin/tc
 #egress port to shape
 PORT=3000
 # Rate to throttle to
-RATE=5mbit
+RATE=2.5mbit
 # Peak rate to allow
-PEAKRATE=6mbit
+PEAKRATE=3mbit
 # Interface to shape
 IF=eth0
 # Average to delay packets by
@@ -60,7 +69,7 @@ IF=eth0
 LATENCY=100ms
 # Jitter value for packet delay
 # Packets will be delayed by $LATENCY +/- $JITTER
-JITTER=5ms
+JITTER=2ms
 
 #buffer should be > RATE/HZ example  For 10mbit/s on Intel(1000HZ), you need at least 10kbyte buffer if you want to reach your configured rate
 #on sns HZ seems to be 125 so for 3MB/s => 3MB/s/125 = 24 kb, double it to be sure
@@ -73,18 +82,16 @@ MTU=2000
 MODEMQ=60ms
 
 
-
 start() {
     #root prio creates classes 1:1, 1:2, and 1:3 (can't create just two bands, 1:3 will be unused)
     $TC qdisc add dev $IF root handle 1: prio
     #port PORT sent to class 1:1
     $TC filter add dev $IF protocol ip parent 1: prio 1 u32 match ip sport $PORT 0xffff flowid 1:1
-    $TC filter add dev $IF protocol ip parent 1: prio 1 u32 match ip sport 3001 0xffff flowid 1:1
     #all other traffic sent to class 1:2
     $TC filter add dev $IF protocol ip parent 1: prio 2 u32 match ip src 0/0 flowid 1:2
     
     #attach shapings to class 1:1
-    $TC qdisc add dev $IF parent 1:1 handle 10: tbf rate $RATE burst $BUFFER limit 120kbit peakrate $PEAKRATE mtu $MTU
+    $TC qdisc add dev $IF parent 1:1 handle 10: tbf rate $RATE burst $BUFFER latency $MODEMQ peakrate $PEAKRATE mtu $MTU
     $TC qdisc add dev $IF parent 10:1 handle 101: netem delay $LATENCY $JITTER
 #    $TC qdisc add dev $IF root handle 1:0 netem delay $LATENCY $JITTER
 #    $TC qdisc add dev $IF parent 1:1 handle 10: tbf rate $RATE burst $BUFFER latency $MODEMQ peakrate $PEAKRATE mtu $MTU
