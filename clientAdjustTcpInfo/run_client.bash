0883d634dcf67b503df55804ad7b99d09d255962
Matvey Arye <arye@cs.princeton.edu>
2014-08-14 17:23:56 -0400

    add scripts

--- clientAdjustTcpInfo/run_client.bash
+++ clientAdjustTcpInfo/run_client.bash
@@ -10,6 +10,8 @@ mkdir $DATADIR
 
 NUMCHUNKS=100
 
+git log --pretty=short -10 > $DATADIR/gitversion
+git diff > $DATADIR/gitdiff
 go run client.go -addr sns58.cs.princeton.edu:3000 -numChunks $NUMCHUNKS -msBetweenChunks $PAUSE $CLIENTARGS 2>&1| tee $DATADIR/client.out
 #sleep 10
 #sudo kill $TCPD
